058def94126a628921a9e679eae6aa9ae8a0fd07
Affan_Pathan <91607963+AffanPathan02@users.noreply.github.com>
2023-11-04 17:02:37 +0530

    Update Knapsack01.java

--- DAA/Knapsack01.java
+++ DAA/Knapsack01.java
@@ -1,33 +1,45 @@
-import java.util.*;
-import java.lang.*;
+import java.util.Scanner;
+
 public class Knapsack01 {
-   public static int findMax(int n1, int n2) {
-      if(n1>n2) {
-         return n1;
-      } else {
-         return n2;
-      }
-   }
-   public static int knapsack(int W, int wt[], int val[], int n) {
-      int K[][] = new int[n+1][W+1];
-      for(int i = 0; i<=n; i++) {
-         for(int w = 0; w<=W; w++) {
-            if(i == 0 || w == 0) {
-               K[i][w] = 0;
-            } else if(wt[i-1] <= w) {
-               K[i][w] = findMax(val[i-1] + K[i-1][w-wt[i-1]], K[i-1][w]);
-            } else {
-               K[i][w] = K[i-1][w];
+    public static int knapsack(int W, int[] wt, int[] val, int len) {
+        int[][] dp = new int[len + 1][W + 1];
+
+        for (int i = 0; i <= len; i++) {
+            for (int w = 0; w <= W; w++) {
+                if (i == 0 || w == 0) {
+                    dp[i][w] = 0;
+                } else if (wt[i - 1] <= w) {
+                    dp[i][w] = Math.max(val[i - 1] + dp[i - 1][w - wt[i - 1]], dp[i - 1][w]);
+                } else {
+                    dp[i][w] = dp[i - 1][w];
+                }
             }
-         }
-      }
-      return K[n][W];
-   }
-   public static void main(String[] args) {
-      int[] val = {70, 20, 50};
-      int[] wt = {11, 12, 13};
-      int W = 30;
-      int len = val.length;
-      System.out.print("Maximum Profit achieved with this knapsack: " + knapsack(W, wt, val, len));
-   }
+        }
+
+        return dp[len][W];
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the number of items: ");
+        int len = scanner.nextInt();
+
+        int[] val = new int[len];
+        int[] wt = new int[len];
+
+        System.out.println("Enter the values and weights of the items:");
+        for (int i = 0; i < len; i++) {
+            System.out.print("Value for item " + (i + 1) + ": ");
+            val[i] = scanner.nextInt();
+            System.out.print("Weight for item " + (i + 1) + ": ");
+            wt[i] = scanner.nextInt();
+        }
+
+        System.out.print("Enter the maximum weight (W) of the knapsack: ");
+        int W = scanner.nextInt();
+
+        int maxProfit = knapsack(W, wt, val, len);
+        System.out.println("Maximum Profit achieved with this knapsack: " + maxProfit);
+    }
 }
